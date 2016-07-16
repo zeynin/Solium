0e370d21684588e14f23d5580779d1e3c7f49cfb
Zeynin <zeynin@hotmail.com>
2016-07-16 11:16:24 -0600

    Initial commit

---
+++ src/test/java/StockInvestmentTest.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Zeynin on 2016-07-15.
+ */
+
+import org.testng.annotations.*;
+
+/**
+ *
+ */
+public class StockInvestmentTest
+{
+    @Test
+    public void testTestStockInvestment() throws Exception
+    {
+
+    }
+
+    @org.testng.annotations.Test
+    public void testStockInvestment() throws Exception
+    {
+
+    }
+
+    public StockInvestmentTest()
+    {
+    }
+}
