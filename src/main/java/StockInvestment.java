3555a5bc80d208b743bbb9029abedefc762d021f
Zeynin <zeynin@hotmail.com>
2016-07-16 20:17:29 -0600

    First round of TDD that works. Added commons.lang3 to help with exceptions while converting from String to Int/Double

---
+++ src/main/java/StockInvestment.java
@@ -0,0 +1,47 @@
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Zeynin on 2016-07-16.
+ */
+public class StockInvestment
+{
+    public String employeeId;
+    public String grantDate;
+    public int shares;
+    public double grantPrice;
+
+    public StockInvestment( String[] record )
+    {
+        // Constructor. Initialize multiplier here, as well as fields
+        // What happens if a string is empty? Enter default values
+        employeeId = "empty";
+        Date now = new Date();
+        SimpleDateFormat date = new SimpleDateFormat( "YYYYMMDD" );
+        grantDate = date.format( now );
+        shares = 0;
+        grantPrice = 0.0;
+
+        String field = record[0].toUpperCase();
+        if( field.equals( "VEST" ) )
+        {
+            employeeId = record[1];
+            grantDate = record[2];
+            shares = NumberUtils.toInt( record[3], 0 );
+            grantPrice = NumberUtils.toDouble( record[4], 0.0 );
+        }
+    }
+
+    public double calculateCashGain( Date marketDate, double marketPrice ) throws Exception
+    {
+        SimpleDateFormat dateFormat = new SimpleDateFormat( "YYYYMMDD" );
+
+        // Verify shares were granted before the market date
+        if( dateFormat.parse( grantDate ).compareTo( marketDate ) > 0 ) return 0.0;
+
+        double holdings = shares * ( marketPrice - grantPrice );
+        return holdings;
+    }
+}
--- src/test/java/StockInvestmentTest.java
+++ src/test/java/StockInvestmentTest.java
@@ -1,27 +1,110 @@
 /**
- * Created by Zeynin on 2016-07-15.
+ * Created by @author Zeynin on 2016-07-15.
  */
 
-import org.testng.annotations.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
  */
 public class StockInvestmentTest
 {
-    @Test
-    public void testTestStockInvestment() throws Exception
+    @org.testng.annotations.Test
+    public static void whenCorrectInputThenNoExceptionIsThrown() throws Exception
     {
+        String[] input = { "VEST", "001B", "20120101", "1000", "0.45" };
+        StockInvestment stockInvestment = new StockInvestment( input );
+    }
+/* handles exception gracefully
+    @org.testng.annotations.Test(expectedExceptions = NumberFormatException.class)
+    public static void whenEmptyInputThenExceptionIsThrown() throws NumberFormatException
+    {
+        String[] input1 = { "VEST", "001B", "20120101", "1000", "" };
+        StockInvestment stockInvestment = new StockInvestment( input1 );
+    }
+*/
+    @org.testng.annotations.Test(expectedExceptions = ParseException.class)
+    public static void whenEmptyInputThenThrowsException() throws Exception
+    {
+        String[] input1 = { "VEST", "001B", "", "1000", "0.45" };
+        StockInvestment stockInvestment = new StockInvestment( input1 );
 
+        SimpleDateFormat dateFormatter = new SimpleDateFormat( "YYYYMMDD" );
+        Date marketDate = dateFormatter.parse( "20130101" );
+        double marketPrice = 1.00;
+
+        System.out.println( stockInvestment.calculateCashGain( marketDate, marketPrice ) );
     }
 
     @org.testng.annotations.Test
-    public void testStockInvestment() throws Exception
+    public static void whenMarketDateIsEqualToGrantDateThenOutputIs500() throws Exception
     {
+        String[] input = { "VEST", "003B", "20130101", "1000", "0.50" };
 
+        StockInvestment stockInvestment = new StockInvestment( input );
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat( "YYYYMMDD" );
+        Date marketDate = dateFormatter.parse( "20130101" );
+        double marketPrice = 1.00;
+
+        System.out.println( stockInvestment.calculateCashGain( marketDate, marketPrice ) );
     }
 
-    public StockInvestmentTest()
+    @org.testng.annotations.Test
+    public static void whenMarketDateIsAfterGrantDateThenOutputIs500() throws Exception
     {
+        String[] input = { "VEST", "003B", "20130101", "1000", "0.50" };
+
+        StockInvestment stockInvestment = new StockInvestment( input );
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat( "YYYYMMDD" );
+        Date marketDate = dateFormatter.parse( "20140101" );
+        double marketPrice = 1.00;
+
+        System.out.println( stockInvestment.calculateCashGain( marketDate, marketPrice ) );
+    }
+
+    @org.testng.annotations.Test
+    public static void whenMarketDateIsBeforeGrantDateThenOutputIs0() throws Exception
+    {
+        String[] input = { "VEST", "003B", "20130101", "1000", "0.50" };
+
+        StockInvestment stockInvestment = new StockInvestment( input );
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat( "YYYYMMDD" );
+        Date marketDate = dateFormatter.parse( "20120101" );
+        double marketPrice = 1.00;
+
+        System.out.println( stockInvestment.calculateCashGain( marketDate, marketPrice ) );
+    }
+
+    @org.testng.annotations.Test(expectedExceptions = ParseException.class)
+    public static void whenGrantDateIsMisformattedThenExceptionThrown() throws Exception
+    {
+        String[] input = { "VEST", "003B", "2013/01/01", "1000", "0.50" };
+
+        StockInvestment stockInvestment = new StockInvestment( input );
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat( "YYYYMMDD" );
+        Date marketDate = dateFormatter.parse( "20120101" );
+        double marketPrice = 1.00;
+
+        System.out.println( stockInvestment.calculateCashGain( marketDate, marketPrice ) );
+    }
+
+    @org.testng.annotations.Test
+    public static void whenVestIsLowercaseThenOutputIs500() throws Exception
+    {
+        String[] input = { "vest", "003B", "20130101", "1000", "0.50" };
+
+        StockInvestment stockInvestment = new StockInvestment( input );
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat( "YYYYMMDD" );
+        Date marketDate = dateFormatter.parse( "20140101" );
+        double marketPrice = 1.00;
+
+        System.out.println( stockInvestment.calculateCashGain( marketDate, marketPrice ) );
     }
 }
